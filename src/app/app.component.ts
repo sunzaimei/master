e06ff17ff9fd750dc2e2efadda0e6b7ea2d37655
sunkaixuan <kaixuan@r-168-103-25-172.comp.nus.edu.sg>
2018-04-22 15:25:48 +0800

    add chart

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
+import { BitcoinService } from './bitcoin.service';
+import { Chart } from 'chart.js';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,54 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  chart =[];
   title = 'app';
+
+  constructor(private _bitcoin:BitcoinService) {}
+  ngOnInit() {
+    this._bitcoin.dailyForecast()
+      .subscribe(res => {
+        let temp_max = res['list'].map(res => res.main.temp_max)
+        let temp_min = res['list'].map(res => res.main.temp_min)
+        let alldates = res['list'].map(res => res.dt)
+
+        let weatherDates = []
+        alldates.forEach((res) => {
+          let jsdate = new Date(res * 1000)
+          weatherDates.push(jsdate.toLocaleTimeString('en', { year: 'numeric', month: 'short', day: 'numeric'}))
+        })
+
+        this.chart = new Chart('canvas', {
+          type: 'line',
+          data: {
+            labels: weatherDates,
+            datasets: [
+              {
+                data: temp_max,
+                borderColor: '#3cba9f',
+                fill: false
+              },
+              {
+                data: temp_min,
+                borderColor: '#ffcc00',
+                fill: false
+              },
+            ]
+          },
+          options: {
+            legend: {
+              display: false
+            },
+            scales: {
+              xAxes: [{
+                display: true
+              }],
+              yAxes: [{
+                display: true
+              }]
+            }
+          }
+        })
+      })
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+// import { FusionChartsModule } from 'angular2-fusioncharts';
 import { AppComponent } from './app.component';
+import { BitcoinService } from './bitcoin.service';
+import { HttpClientModule } from '@angular/common/http';
 
+// import * as FusionCharts from 'fusioncharts';
+// import * as Charts from 'fusioncharts/fusioncharts.charts';
+// import * as Fint from 'fusioncharts/themes/fusioncharts.theme.fint';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    // FusionChartsModule.forRoot(FusionCharts, Charts),
+    HttpClientModule
   ],
-  providers: [],
+  providers: [BitcoinService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
